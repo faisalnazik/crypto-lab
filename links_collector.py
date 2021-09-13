bad6f11553396df28a3a2da5762e85d9e4c6278f
Faisal Nazik <faisalnazik@gmail.com>
2021-09-13 16:47:47 +0500

    Covert to .xlsx file

--- links_collector.py
+++ links_collector.py
@@ -1,15 +1,42 @@
 from bs4 import BeautifulSoup
 import requests
+import re
+# Importing pandas as pd
+import pandas as pd
 
-# r = requests.get('https://naturalmedicineclinic.herokuapp.com/admin')
-# # r.status_code
-# r.headers['content-type']
-# print(r)
-url = "https://github.com/faisalnazik" 
+df = pd.DataFrame()
+
+url = "https://www.house.gov/representatives" 
 text = requests.get(url).text
 soup = BeautifulSoup(text, "html5lib")
 all_urls = [a['href']
 for a in soup('a')
 if a.has_attr('href')]
-print(len(all_urls))# 91 for me, way too many
-print(all_urls)
+
+# Must start with http:// or https://
+# Must end with .house.gov or .house.gov/
+regex = r"^https?://.*\.house\.gov/?$"
+
+# Let's write some tests!
+assert re.match(regex, "http://joel.house.gov")
+assert re.match(regex, "https://joel.house.gov")
+assert re.match(regex, "http://joel.house.gov/")
+assert re.match(regex, "https://joel.house.gov/")
+assert not re.match(regex, "joel.house.gov")
+assert not re.match(regex, "http://joel.house.com")
+assert not re.match(regex, "https://joel.house.gov/biography")
+
+
+# And now apply
+good_urls = [url for url in all_urls if re.match(regex, url)]
+
+good_urls = list(set(good_urls))
+print(len(all_urls)) #967 total 
+# print(all_urls)
+print(len(good_urls)) #437 for me the good one
+# print(good_urls)
+
+# Creating two columns
+df['links'] = good_urls[0::1]
+# Converting to excel
+df.to_excel('result.xlsx', index = True)
