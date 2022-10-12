672c44aa441f3653cb661e86725c5bc781f5a5c0
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-12 15:00:42 +0000

    Adding tarfile member sanitization to extractall()

--- spam_identifier/spam_data_download.py
+++ spam_identifier/spam_data_download.py
@@ -24,7 +24,29 @@ for filename in FILES:
     fin = BytesIO(content)
     # And extract all the files to the specified output dir.
     with tarfile.open(fileobj=fin, mode='r:bz2') as tf:
-        tf.extractall(OUTPUT_DIR)
+        
+        import os
+        
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(tf, OUTPUT_DIR)
 
 # modify the path to wherever you've put the files
 path = 'spam_data/*/*'
